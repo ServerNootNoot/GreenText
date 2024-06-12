5810148c651ba5974a3a5c07270db617d18aa40f
ServerNootNoot <servernootnoot@gmail.com>
2024-06-12 17:39:29 -0400

    Cringe

---
+++ src/main/java/lol/nightshade/greentext/GreenText.java
@@ -0,0 +1,17 @@
+package lol.nightshade.greentext;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GreenText extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
